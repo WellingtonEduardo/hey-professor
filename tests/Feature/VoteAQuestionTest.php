305ffa5900b0f5085aeef0c58e62acd46109989d
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-10-12 00:25:59 -0300

    HEY-15: Adding the unlike button

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -44,3 +44,44 @@ it(
 
     }
 );
+
+it(
+    'should be able to unlike a question',
+    function () {
+        /** @var User $user */
+        $user = User::factory()->create();
+        actingAs($user);
+
+        $question = Question::factory()->create();
+
+        post(route('question.unlike', $question))->assertRedirect();
+
+        assertDatabaseHas('votes', [
+            'question_id' => $question->id,
+            'unlike'      => 0,
+            'unlike'      => 1,
+            'user_id'     => $user->id,
+        ]);
+
+    }
+);
+
+it(
+    'should not be able to unlike more than one time',
+    function () {
+        /** @var User $user */
+        $user = User::factory()->create();
+        actingAs($user);
+
+        $question = Question::factory()->create();
+
+        post(route('question.unlike', $question));
+        post(route('question.unlike', $question));
+        post(route('question.unlike', $question));
+        post(route('question.unlike', $question));
+
+        expect($user->votes()->where('question_id', '=', $question->id)->get())
+            ->toHaveCount(1);
+
+    }
+);
