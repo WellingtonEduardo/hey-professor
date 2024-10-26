3312c35c2fe6603894bbd1fbee606523c3d3a5e9
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-10-26 18:39:41 -0300

    HEY-28: applying filter in the front end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,14 +7,43 @@
 
     <x-container>
 
-        <div class="mb-2 font-bold uppercase dark:text-gray-400">Lista de perguntas</div>
+        <form action="{{ route('dashboard') }}" method="GET" class="flex space-x-2">
+            @csrf
+            <x-text-input type="text" name="search" value="{{ request()->search }}" class="flex-1" />
+            <x-btn.primary type="submit">
+                Search
+            </x-btn.primary>
+        </form>
 
-        <div class="space-y-4 dark:text-gray-400">
-            @foreach ($questions as $item)
-                <x-question :question="$item" />
-            @endforeach
 
-            {{ $questions->withQueryString()->links() }}
+
+        <div class="mt-4 space-y-4 dark:text-gray-400">
+
+            @if ($questions->isEmpty())
+                <div class="m-auto flex justify-center dark:text-gray-300">
+
+                    <div class="text-center">
+                        <x-draw.searching width="300" />
+
+                        <p class="mt-6 text-2xl dark:text-gray-300">
+                            No questions found.
+                        </p>
+                    </div>
+
+                </div>
+            @else
+                @foreach ($questions as $item)
+                    <x-question :question="$item" />
+                @endforeach
+
+                {{ $questions->withQueryString()->links() }}
+
+            @endif
+
+
+
+
+
         </div>
 
     </x-container>
